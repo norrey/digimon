a51f7e56a4b9733bc8d8aec76d4a000a1a8dc9df
Norrey OKumu <okumu.norrey@gmail.com>
2018-04-17 14:51:47 +0300

    Added sample comment

--- src/main/java/com/norrey/puzzle/Application.java
+++ src/main/java/com/norrey/puzzle/Application.java
@@ -3,7 +3,7 @@ package com.norrey.puzzle;
 import com.norrey.puzzle.game.Launcher;
 
 /**
- *
+ * This is the entry point of this project
  * @author Norrey Okumu <okumu.norrey@gmail.com>
  * @since Feb 16, 2018, 10:12:39 AM
  */
