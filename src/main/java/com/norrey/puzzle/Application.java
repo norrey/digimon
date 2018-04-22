e82bbca0271f7f7fb0bd9ad819a57b46296cab9f
Norrey OKumu <okumu.norrey@gmail.com>
2018-04-23 00:52:27 +0300

    Removed unnecessary comment

--- src/main/java/com/norrey/puzzle/Application.java
+++ src/main/java/com/norrey/puzzle/Application.java
@@ -4,7 +4,6 @@ import com.norrey.puzzle.game.Launcher;
 
 /**
  * This is the entry point of this project
- * Whatever made me fail this point is use of static methods
  *
  * @author Norrey Okumu <okumu.norrey@gmail.com>
  * @since Feb 16, 2018, 10:12:39 AM
