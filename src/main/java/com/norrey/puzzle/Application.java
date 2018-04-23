5bbe3105eea68095675eaa87eafe635fe30d53e8
Norrey OKumu <okumu.norrey@gmail.com>
2018-04-23 09:47:42 +0300

    Jenkis Test

--- src/main/java/com/norrey/puzzle/Application.java
+++ src/main/java/com/norrey/puzzle/Application.java
@@ -5,6 +5,7 @@ import com.norrey.puzzle.game.Launcher;
 /**
  * This is the entry point of this project
  *
+ * 
  * @author Norrey Okumu <okumu.norrey@gmail.com>
  * @since Feb 16, 2018, 10:12:39 AM
  */
